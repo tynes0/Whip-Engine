6503a50d4833015dfab7086e41cf4af2f92bd31a
tynes0 <cihanbilgihan@gmail.com>
2026-06-21 19:08:57 +0300

    Bump Rider plugin version

--- tools/WhipToolsRider/build.gradle.kts
+++ tools/WhipToolsRider/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.whip"
-version = "0.1.0"
+version = "0.1.1"
 
 val whipBuildRoot = providers.gradleProperty("whipBuildRoot")
 val localRiderPath = providers.gradleProperty("localRiderPath")
